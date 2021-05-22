a83730c09325bc91a93bf32bbc075e34cd315903
Cristopher Ortega <ortega.cpp@gmail.com>
2021-05-22 04:42:59 -0400

    Update matic factory address

--- src/commons/addresses.ts
+++ src/commons/addresses.ts
@@ -8,7 +8,7 @@ export function getFactoryAddress(): string {
   // not using a switch-case because using strings is not yet supported (only u32)
   if (network == 'mainnet') return '0xd34971bab6e5e356fd250715f5de0492bb070452'
   if (network == 'xdai') return '0xa818b4f111ccac7aa31d0bcc0806d64f2e0737d7'
-  if (network == 'matic') return '0x6f937495013c7dc42af752d3e0bcc090bd34f7ab'
+  if (network == 'matic') return '0x03daa61d8007443a6584e3d8f85105096543c19c'
   log.warning('no factory address for unsupported network {}', [network])
   return ADDRESS_ZERO
 }
