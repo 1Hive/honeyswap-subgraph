dd8baa1c285c3412eff525ae3119cf2e8f5c567a
Felipe Novaes F Rocha <windholyghost@gmail.com>
2023-04-01 17:53:18 -0300

    fix: remove commented code

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -6,7 +6,6 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        // url: "https://xdai-archive.blockscout.com",
         url: 'https://gno.getblock.io/mainnet/?api_key=',
         blockNumber: 11813490
       }
